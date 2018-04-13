1c92f6ea6a4e18bd710898a0408ecafa25460689
Sarthak <sarthak.dubey@outlook.com>
2018-04-13 02:04:01 -0700

    Update

---
+++ LinkedList.py
@@ -0,0 +1,78 @@
+class Node(object):
+	"""Node"""
+
+	def __init__(self, data=None, next_node=None):
+		#super(Node, self).__init__()
+		self.data = data
+		self.next_node = next_node
+
+	def get_data(self):
+		return self.data
+
+	def get_next(self):
+		return self.next_node
+
+	def set_next(self, new_next):
+		self.next_node = new_next
+
+
+class LinkedList(object):
+	"""LinkedList"""
+
+	def __init__(self, head=None):
+		#super(LinkedList, self).__init__()
+		self.head = head
+
+	def insert(self, data):
+		new_node = Node(data)
+		new_node.set_next(self.head)
+		self.head = new_node
+
+	def size(self):
+		current = self.head
+		count = 0
+		while current:
+			count += 1
+			current = current.get_next()
+		return count
+
+	def search(self, data):
+		current = self.head
+		found = False
+		while current and found is False:
+			if current.get_data() == data:
+				found = True
+			else:
+				current = current.get_next()
+			if current is None:
+				raise ValueError("Data not in list")
+		return current
+
+	def delete(self, data):
+		current = self.head
+		previous = None
+		found = False
+		while current and found is False:
+			if current.get_data() == data:
+				found = True
+			else:
+				previous = current
+				current = current.get_next()
+		if current is None:
+			raise ValueError("Data not in list")
+		if previous is None:
+			self.head = current.get_next()
+		else:
+			previous.set_next(current.get_next())
+
+def main():
+	print('Main Executed')
+	l1 = LinkedList()
+	l1.insert(42)
+	l1.insert(23)
+
+	print(l1.size())
+
+if __name__ == '__main__':
+	main()
+
